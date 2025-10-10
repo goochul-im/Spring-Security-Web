44c4734ca6a1d933e21ea37949f1543da8cd4452
goochul-im <goochul175465@gmail.com>
2025-10-10 18:29:13 +0900

    :tada: feat(*): start project

---
+++ src/main/java/com/springsecurityjsw/SpringSecurityJswApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurityjsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJswApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJswApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springsecurityjsw/SpringSecurityJswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurityjsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJswApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
