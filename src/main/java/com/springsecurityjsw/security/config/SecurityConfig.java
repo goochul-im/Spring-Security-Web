41a740343757ca5dfa74de68763bd9ffcb4d69f8
goochul-im <goochul175465@gmail.com>
2025-10-15 22:07:40 +0900

    feat(Login): 로그인 실패 로직 추가

--- src/main/java/com/springsecurityjsw/security/config/SecurityConfig.java
+++ src/main/java/com/springsecurityjsw/security/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
@@ -21,18 +22,20 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
     private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
     private final AuthenticationSuccessHandler successHandler;
+    private final AuthenticationFailureHandler failureHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
-                        .requestMatchers("/", "/signup", "/user/signup").permitAll()
+                        .requestMatchers("/", "/signup", "/user/signup","/login*").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .authenticationDetailsSource(authenticationDetailsSource)
                         .loginPage("/login")
                         .successHandler(successHandler)
+                        .failureHandler(failureHandler)
                         .permitAll()
                 )
                 .authenticationProvider(authenticationProvider);
---
+++ src/main/java/com/springsecurityjsw/security/handler/FormAuthenticationFailureHandler.java
@@ -0,0 +1,36 @@
+package com.springsecurityjsw.security.handler;
+
+import com.springsecurityjsw.security.exception.SecretException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class FormAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+        String errorMessage = "invalid username or password";
+        if (exception instanceof BadCredentialsException) {
+            errorMessage = "invalid username or password";
+        } else if (exception instanceof UsernameNotFoundException) {
+            errorMessage = "user not exist";
+        } else if (exception instanceof CredentialsExpiredException) {
+            errorMessage = "Expired password";
+        } else if (exception instanceof SecretException) {
+            errorMessage = "invalid secret key";
+        }
+        setDefaultFailureUrl("/login?error=true&exception="+errorMessage);
+
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
